5e640e260d7fc986342e0aad190d0f3f4e54c17e
SDILIPS <dilip.sridhar.c@gmail.com>
2020-05-05 14:49:29 +0200

    Flatten Linked List
    Flatten Linked List

---
+++ class_code/LinkedList/FlattenLinkedList.py
@@ -0,0 +1,88 @@
+# Use this class as the nodes in your linked list
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+    def __repr__(self):
+        return str(self.value)
+    
+class LinkedList:
+    def __init__(self, head):
+        self.head = head
+        
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+        node = self.head
+        while node.next is not None:
+            node = node.next
+        node.next = Node(value)
+
+def merge(list1, list2):
+    # TODO: Implement this function so that it merges the two linked lists in a single, sorted linked list.
+    merged = LinkedList(None)
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
+    list1_ptr = list1.head
+    list2_ptr = list2.head
+    while list1_ptr is not None or list1_ptr is not None:
+        if list1_ptr is None:
+            merged.append(list2_ptr)
+            list2_ptr = list2_ptr.next
+        elif list2_ptr is None:
+            merged.append(list1_ptr)
+            list1_ptr = list1_ptr.next
+        elif list1_ptr.value < list2_ptr.value:
+            merged.append(list1_ptr)
+            list1_ptr = list1_ptr.next
+        else:
+            merged.append(list2_ptr)
+            list2_ptr = list2_ptr.next
+    return merged
+    pass
+
+linked_list = LinkedList(Node(1))
+linked_list.append(3)
+linked_list.append(5)
+
+second_linked_list = LinkedList(Node(2))
+second_linked_list.append(4)
+
+merged = merge(linked_list, second_linked_list)
+node = merged.head
+while node is not None:
+    #This will print 1 2 3 4 5
+    print(node.value)
+    node = node.next
+    
+# Lets make sure it works with a None list
+merged = merge(None, linked_list)
+node = merged.head
+while node is not None:
+    #This will print 1 3 5
+    print(node.value)
+    node = node.next
+
+
+class NestedLinkedList(LinkedList):
+    def flatten(self):
+        return self._flatten(self.head)
+
+    def _flatten(self, node):
+        if node.next is None:
+            return merge(node.value, None)
+        return merge(node.value, self._flatten(node.next))
+
+nested_linked_list = NestedLinkedList(Node(linked_list))
+nested_linked_list.append(second_linked_list)
+flattened = nested_linked_list.flatten()
+
+node = flattened.head
+while node is not None:
+    #This will print 1 2 3 4 5
+    print(node.value)
+    node = node.next
